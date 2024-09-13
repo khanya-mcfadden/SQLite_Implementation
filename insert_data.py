e14e1bbe839c2e81532b71d5ed19e101d385cf3c
khanya-mcfadden <23malgasmk037@collyers.ac.uk>
2024-09-13 09:48:07 +0100

    Refactor SQL table creation and update sales.html template

---
+++ insert_data.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+connection = sqlite3.connect('customer.db', check_same_thread=False) #Will create DB if it doesn't already exist
+
+query = """INSERT INTO customer (username, email, First_Name, Last_Name, address, Phone_Number) VALUES ('johndoe', 'john@gmail.com', 'John', 'Doe', '1234 Elm St', '555-555-5555');"""
+cursor = connection.cursor()
+cursor.execute(query)
+connection.commit()  # Commit the transaction
+cursor.close()
