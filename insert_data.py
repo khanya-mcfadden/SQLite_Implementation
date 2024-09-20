c082faf9c0fdc17fb60bdc40c9c3cba943960754
khanya-mcfadden <23malgasmk037@collyers.ac.uk>
2024-09-20 08:55:08 +0100

    Update database connection and insert customer data

--- insert_data.py
+++ insert_data.py
@@ -1,17 +1,17 @@
 import sqlite3
 
-# connection = sqlite3.connect('customer.db', check_same_thread=False) #Will create DB if it doesn't already exist
+connection = sqlite3.connect('BeanBrew.db', check_same_thread=False) #Will create DB if it doesn't already exist
 
-# query = """INSERT INTO customer (username, email, First_Name, Last_Name, address, Phone_Number) VALUES ('johndoe', 'john@gmail.com', 'John', 'Doe', '1234 Elm St', '555-555-5555');"""
-# cursor = connection.cursor()
-# cursor.execute(query)
-# connection.commit()  # Commit the transaction
-# cursor.close()
-
-
+query = """INSERT INTO customer (FirstName, LastName, email, Phone) VALUES ('John', 'Doe', 'john@gmail.com', '555-555-5555');"""
+cursor = connection.cursor()
+cursor.execute(query)
+connection.commit()  # Commit the transaction
+cursor.close()
 
 
-connection = sqlite3.connect('sale.db', check_same_thread=False) #Will create DB if it doesn't already exist
+
+
+connection = sqlite3.connect('BeanBrew.db', check_same_thread=False) #Will create DB if it doesn't already exist
 query = """INSERT INTO sale (productId, customerId, date, time) VALUES ('1','1','20/12/2024', '16:00');"""
 cursor = connection.cursor()
 cursor.execute(query)
