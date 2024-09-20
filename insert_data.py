f5d77a1710844d450654edd844fb051b92269167
khanya-mcfadden <23malgasmk037@collyers.ac.uk>
2024-09-20 10:00:05 +0100

    Refactor insert_data.py to update customer and sale data

--- insert_data.py
+++ insert_data.py
@@ -2,7 +2,7 @@ import sqlite3
 
 connection = sqlite3.connect('BeanBrew.db', check_same_thread=False) #Will create DB if it doesn't already exist
 
-query = """INSERT INTO customer (FirstName, LastName, email, Phone) VALUES ('John', 'Doe', 'john@gmail.com', '555-555-5555');"""
+query = """INSERT INTO customer (FirstName, LastName, email, Phone) VALUES ('sam', 'mathews', 'samm@gmail.com', '501-346-439');"""
 cursor = connection.cursor()
 cursor.execute(query)
 connection.commit()  # Commit the transaction
@@ -10,9 +10,8 @@ cursor.close()
 
 
 
-
 connection = sqlite3.connect('BeanBrew.db', check_same_thread=False) #Will create DB if it doesn't already exist
-query = """INSERT INTO sale (productId, customerId, date, time) VALUES ('1','1','20/12/2024', '16:00');"""
+query = """INSERT INTO sale (productId, customerId, date, time) VALUES ('4','1','20/12/2024', '16:00');"""
 cursor = connection.cursor()
 cursor.execute(query)
 connection.commit()  # Commit the transaction
