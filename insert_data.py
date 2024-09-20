775805af5f2a7cfd5cecb5ffde279ef0bfe2f7bc
khanya-mcfadden <23malgasmk037@collyers.ac.uk>
2024-09-20 10:21:47 +0100

    added the ability to gen random data

--- insert_data.py
+++ insert_data.py
@@ -1,14 +1,42 @@
 import sqlite3
+import random
+import faker
+fake = faker.Faker()
 
 connection = sqlite3.connect('BeanBrew.db', check_same_thread=False) #Will create DB if it doesn't already exist
 
-query = """INSERT INTO customer (FirstName, LastName, email, Phone) VALUES ('sam', 'mathews', 'samm@gmail.com', '501-346-439');"""
-cursor = connection.cursor()
-cursor.execute(query)
-connection.commit()  # Commit the transaction
-cursor.close()
 
 
+
+def generate_random_customer():
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+    email = fake.email()
+    phone = fake.phone_number()
+    return (first_name, last_name, email, phone)
+
+def generate_random_sale():
+    product_id = random.randint(1, 10)  # Assuming product IDs range from 1 to 10
+    customer_id = random.randint(1, 10)  # Assuming customer IDs range from 1 to 10
+    date = fake.date(pattern="%d/%m/%Y")
+    time = fake.time(pattern="%H:%M")
+    return (product_id, customer_id, date, time)
+for _ in range(5):
+    # Insert random customer data
+    random_customer = generate_random_customer()
+    query = """INSERT INTO customer (FirstName, LastName, email, Phone) VALUES (?, ?, ?, ?);"""
+    cursor = connection.cursor()
+    cursor.execute(query, random_customer)
+    connection.commit()  # Commit the transaction
+    cursor.close()
+
+    # Insert random sale data
+    random_sale = generate_random_sale()
+    query = """INSERT INTO sale (productId, customerId, date, time) VALUES (?, ?, ?, ?);"""
+    cursor = connection.cursor()
+    cursor.execute(query, random_sale)
+    connection.commit()  # Commit the transaction
+    cursor.close()
 
 connection = sqlite3.connect('BeanBrew.db', check_same_thread=False) #Will create DB if it doesn't already exist
 query = """INSERT INTO sale (productId, customerId, date, time) VALUES ('4','1','20/12/2024', '16:00');"""
