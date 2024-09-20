4fbf333893b7252bcbb43e37714c2015c54562a0
khanya-mcfadden <23malgasmk037@collyers.ac.uk>
2024-09-20 10:29:13 +0100

    re-did database

--- insert_data.py
+++ insert_data.py
@@ -21,10 +21,31 @@ def generate_random_sale():
     date = fake.date(pattern="%d/%m/%Y")
     time = fake.time(pattern="%H:%M")
     return (product_id, customer_id, date, time)
+
 def generate_random_product():
     product_name = fake.word()
+    product_name = fake.random_element(elements=("Coffee", "Tea", "Muffin", "Sandwich", "Cake", "Cookie", "Brownie", "Bagel", "Croissant", "Smoothie",))
+    "Latte", "Espresso", "Cappuccino", "Americano", "Macchiato", "Mocha", "Flat White", "Affogato", "Iced Coffee", "Cold Brew",
+    "Green Tea", "Black Tea", "Herbal Tea", "Chai Latte", "Matcha Latte", "Iced Tea", "Bubble Tea", "Lemonade", "Hot Chocolate", "Milkshake",
+    "Blueberry Muffin", "Chocolate Muffin", "Banana Muffin", "Bran Muffin", "Corn Muffin", "Pumpkin Muffin", "Apple Muffin", "Carrot Muffin", "Lemon Poppy Seed Muffin", "Cranberry Muffin",
+    "Turkey Sandwich", "Ham Sandwich", "Chicken Sandwich", "Veggie Sandwich", "BLT Sandwich", "Club Sandwich", "Grilled Cheese Sandwich", "Egg Salad Sandwich", "Tuna Sandwich", "Roast Beef Sandwich",
+    "Cheesecake", "Chocolate Cake", "Carrot Cake", "Red Velvet Cake", "Lemon Cake", "Pound Cake", "Sponge Cake", "Angel Food Cake", "Bundt Cake", "Coffee Cake",
+    "Chocolate Chip Cookie", "Oatmeal Raisin Cookie", "Peanut Butter Cookie", "Sugar Cookie", "Snickerdoodle", "Gingerbread Cookie", "Macaron", "Shortbread Cookie", "Biscotti", "Fortune Cookie",
+    "Fudge Brownie", "Walnut Brownie", "Blondie", "Cheesecake Brownie", "Peanut Butter Brownie", "Mint Brownie", "Caramel Brownie", "Raspberry Brownie", "Marble Brownie", "Espresso Brownie",
+    "Plain Bagel", "Everything Bagel", "Sesame Bagel", "Cinnamon Raisin Bagel", "Blueberry Bagel", "Poppy Seed Bagel", "Onion Bagel", "Garlic Bagel", "Salt Bagel", "Cheddar Bagel",
+    "Butter Croissant", "Almond Croissant", "Chocolate Croissant", "Ham and Cheese Croissant", "Spinach and Feta Croissant", "Plain Croissant", "Raspberry Croissant", "Blueberry Croissant", "Apple Croissant", "Strawberry Croissant",
+    "Strawberry Smoothie", "Banana Smoothie", "Mango Smoothie", "Berry Smoothie", "Green Smoothie", "Peach Smoothie", "Pineapple Smoothie", "Avocado Smoothie", "Protein Smoothie", "Detox Smoothie",
+    "Granola Bar", "Protein Bar", "Fruit Bar", "Nut Bar", "Chocolate Bar", "Energy Bar", "Cereal Bar", "Yogurt Bar", "Oat Bar", "Seed Bar",
+    "Greek Yogurt", "Vanilla Yogurt", "Strawberry Yogurt", "Blueberry Yogurt", "Peach Yogurt", "Mango Yogurt", "Coconut Yogurt", "Almond Yogurt", "Soy Yogurt", "Cashew Yogurt",
+    "Apple", "Banana", "Orange", "Grapes", "Strawberries", "Blueberries", "Raspberries", "Blackberries", "Pineapple", "Mango",
+    "Almonds", "Cashews", "Walnuts", "Pistachios", "Peanuts", "Hazelnuts", "Macadamia Nuts", "Brazil Nuts", "Pecans", "Chestnuts",
+    "Cheddar Cheese", "Swiss Cheese", "Mozzarella Cheese", "Parmesan Cheese", "Brie Cheese", "Gouda Cheese", "Blue Cheese", "Feta Cheese", "Goat Cheese", "Provolone Cheese",
+    "Sparkling Water", "Still Water", "Coconut Water", "Vitamin Water", "Flavored Water", "Mineral Water", "Spring Water", "Distilled Water", "Purified Water", "Alkaline Water",
+    "Orange Juice", "Apple Juice", "Grape Juice", "Cranberry Juice", "Pineapple Juice", "Tomato Juice", "Carrot Juice", "Beet Juice", "Pomegranate Juice", "Mango Juice",
+    "Potato Chips", "Tortilla Chips", "Pretzels", "Popcorn", "Cheese Puffs", "Corn Chips", "Veggie Chips", "Pita Chips", "Rice Cakes", "Crackers",
+    "Chicken Soup", "Tomato Soup", "Minestrone Soup", "Clam Chowder", "Broccoli Cheddar Soup", "French Onion Soup", "Lentil Soup", "Miso Soup", "Beef Stew", "Chicken Noodle Soup"
     product_description = fake.sentence()
-    price = random.uniform(1.0, 100.0)
+    price = round(random.uniform(1.0, 100.0), 2)
     return (product_name, product_description, price)
 
 for _ in range(200):
